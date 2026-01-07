537b162a64cd711f1ca39a1a8b68b945574ffde1
Monish S <monishs9778@gmail.com>
2026-01-07 23:27:31 +0530

    Create model.py

---
+++ src/model.py
@@ -0,0 +1,19 @@
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+def train_model(df):
+    X = df[['Age', 'MonthlyIncome', 'YearsAtCompany', 'WorkHoursPerWeek']]
+    y = df['Attrition']
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
+
+    model = LogisticRegression()
+    model.fit(X_train, y_train)
+
+    predictions = model.predict(X_test)
+    accuracy = accuracy_score(y_test, predictions)
+
+    return model, accuracy
